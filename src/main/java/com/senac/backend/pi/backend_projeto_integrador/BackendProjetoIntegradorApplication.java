30d4f45cc64e619f6996d1d94a87a9eb91dcb544
KaikSAlves <souzaalveskaik@gmail.com>
2025-10-02 13:58:11 -0300

    Subindo projeto

---
+++ src/main/java/com/senac/backend/pi/backend_projeto_integrador/BackendProjetoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.senac.backend.pi.backend_projeto_integrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjetoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjetoIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/backend/pi/backend_projeto_integrador/BackendProjetoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.backend.pi.backend_projeto_integrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjetoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
